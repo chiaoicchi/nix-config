ff47b2b13b00349d040338ff8df81e3140211a78
chiaoicchi <182973681+chiaoicchi@users.noreply.github.com>
2026-01-17 15:02:54 +0900

    apply format

--- modules/home/neovim/lua/lsp.lua
+++ modules/home/neovim/lua/lsp.lua
@@ -1,19 +1,26 @@
 local keymap = vim.keymap.set
 
-vim.api.nvim_create_autocmd('LspAttach', {
+vim.api.nvim_create_autocmd("LspAttach", {
   callback = function(args)
     local bufnr = args.buf
     local client = vim.lsp.get_client_by_id(args.data.client_id)
-
 
     keymap("n", "gd", vim.lsp.buf.definition)
     keymap("n", "gr", vim.lsp.buf.references)
     keymap("n", "K", vim.lsp.buf.hover)
 
-    keymap("n", "[d", function() vim.diagnostic.jump({ count = -1 }) end)
-    keymap("n", "]d", function() vim.diagnostic.jump({ count = 1 }) end)
-    keymap("n", "[e", function() vim.diagnostic.jump({ count = -1, severity = vim.diagnostic.severity.ERROR }) end)
-    keymap("n", "]e", function() vim.diagnostic.jump({ count = 1, severity = vim.diagnostic.severity.ERROR }) end)
+    keymap("n", "[d", function()
+      vim.diagnostic.jump({ count = -1 })
+    end)
+    keymap("n", "]d", function()
+      vim.diagnostic.jump({ count = 1 })
+    end)
+    keymap("n", "[e", function()
+      vim.diagnostic.jump({ count = -1, severity = vim.diagnostic.severity.ERROR })
+    end)
+    keymap("n", "]e", function()
+      vim.diagnostic.jump({ count = 1, severity = vim.diagnostic.severity.ERROR })
+    end)
 
     if client and client.supports_method("textDocument/formatting") then
       vim.api.nvim_create_autocmd("BufWritePre", {
@@ -26,9 +33,9 @@ vim.api.nvim_create_autocmd('LspAttach', {
   end,
 })
 
-vim.lsp.config('lua_ls', {
-  cmd = { 'lua-language-server' },
-  filetypes = { 'lua' },
+vim.lsp.config("lua_ls", {
+  cmd = { "lua-language-server" },
+  filetypes = { "lua" },
   settings = {
     Lua = {
       diagnostics = { globals = { "vim" } },
@@ -37,9 +44,9 @@ vim.lsp.config('lua_ls', {
     },
   },
 })
-vim.lsp.config('nil_ls', {
-  cmd = { 'nil' },
-  filetypes = { 'nix' },
+vim.lsp.config("nil_ls", {
+  cmd = { "nil" },
+  filetypes = { "nix" },
 })
-vim.lsp.enable('lua_ls')
-vim.lsp.enable('nil_ls')
+vim.lsp.enable("lua_ls")
+vim.lsp.enable("nil_ls")
